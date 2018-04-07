9d06fbda386175d88aba3fe416ffd8950fd9e64b
root <root@ip-172-31-84-133.ec2.internal>
2018-04-07 10:57:15 +0000

    test

---
+++ userdata.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+yum update -y
+yum install -y httpd24 
+service httpd start
+chkconfig httpd on
+groupadd www
+usermod -a -G www ec2-user
+chown -R root:www /var/www
+chmod 2775 /var/www
+find /var/www -type d -exec chmod 2775 {} +
+find /var/www -type f -exec chmod 0664 {} +
+echo "this is webserver " > /var/www/html/index.html
+
+
